23c83eb3e4c9f0b47f3856add959d7e956823468
Administrator <651335311@qq.com>
2026-02-19 17:49:19 +0800

    好了，之前包括现在都是在练习

--- 练习.py
+++ 练习.py
@@ -1,35 +1,8 @@
-class Mammal:
-    def __init__(self, name, sex):
-        self.name = name
-        self.sex = sex
-        self.num_eyes = 2
-    
-    def breathe(self):
-        print(self.name + "在呼吸...")
-    
-    def poop(self):
-        print(self.name + "在拉屎...")
+# 类继承练习：人力系统
+# - 员工分为两类：全职员工 FullTimeEmployee、兼职员工 PartTimeEmployee。
+# - 全职和兼职都有“姓名 name”、“工号 id”属性，
+#   都具备“打印信息 print_info”（打印姓名、工号）方法。
+# - 全职有“月薪 monthly_salary”属性，
+#   兼职有“日薪 daily_salary”属性、“每月工作天数 work_days”的属性。
+# - 全职和兼职都有“计算月薪 calculate_monthly_pay”的方法，但具体计算过程不一样。
 
-class Human(Mammal):
-    def __init__(self, name, sex):
-        super().__init__(name, sex)
-        self.has_tail = False
-    
-    def resd(self):
-        print(self.name + "在阅读...")
-
-
-class Cat(Mammal):
-    def __init__(self, name, sex):
-        super().__init__(name, sex)
-        self.has_tail = True
-    
-    def scratch_sofa(self):
-        print(self.name + "在抓沙发...")
-    
-    def poop(self):
-        print(self.name + "在猫砂上拉屎")
-
-cat1 = Cat("Jojo", "男性")
-print(f"小猫{cat1.name}是一个{cat1.sex}的猫")
-cat1.poop()
