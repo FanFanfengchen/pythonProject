3ae206070a882ca1acfddb671feebff266f895e0
FanFanfengchen <133505922+FanFanfengchen@users.noreply.github.com>
2026-03-28 23:03:28 +0800

    feat: 添加Python基础示例代码和变量定义
    添加了Python脚本示例代码，包括字符串操作和变量定义

--- Python数据分析/第一篇 Python编程基础/项目1 搭建运动员欢迎系统_基础知识.py
+++ Python数据分析/第一篇 Python编程基础/项目1 搭建运动员欢迎系统_基础知识.py
@@ -6,3 +6,27 @@
 print("ab" in "acbd")
 print("a" not in "acbd")
 print("ac" in "acbd")
+
+# 6页 图1-5
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# 一、变量
+
+spor_1_name = "001"
+# 创建了一个由数字“001”组成的变量，并且给它起了一个变量名spor_1_name
+
